9e46b10f3c0ac67ef1cd4cae9307f794e791b545
Lilia Rigumye <rigumyel@gmail.com>
2014-11-09 20:50:35 +0100

    Uploading plot2.R and plot2.png

---
+++ plot2.R
@@ -0,0 +1,26 @@
+#########Scripts that reads into R a dataset from a csv file, draws a plot out of "Global Active Power" values and Date/Time values#####################################
+#########and saves it as a png file inside the working directory########################################################################################
+plot2 <- function(){
+    ##reads the processed data into R from a csv file containing data for 01/02/07 and 02/02/07 dates only and stores it into a data.frame
+    rawData <- read.csv("exdata-data-household_power_consumption/processeData.csv")
+    ##loads the reshape package
+    library(reshape)
+    ##renames the "Date" variable
+    rawData <- rename(rawData, c(X = "Date"))
+    ##changes the type of "Date" varialbe from factor to Date
+    rawData$Date <- as.Date(rawData$Date, "%d/%m/%Y")
+    # concatenating date and time to convert it to a time format
+    xAxis <-paste(rawData$Date,rawData$Time,sep=' ') 
+    x <- strptime(xAxis,"%Y-%m-%d %H:%M")
+    ##Stores the "Global active power" values into a variable y
+    y <- rawData$Global_active_power    
+    
+    ##opens the screen device on windows
+    windows()  
+    ##creates a histogram and sends it to the screen
+    plot(x, y, type="l", xlab="Days", ylab="Global Active Power(kilowatts)")
+    ##copies the histogram to a "plot2.png" file inside the working directory
+    dev.copy(png, file="plot2.png")
+    ##closes the png device
+    dev.off()
+}
